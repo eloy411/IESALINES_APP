3c8b50cb2890f8361377d53d4d08c354d4b5d731
Miriam Bonillo <miriam.bonillosmx@gmail.com>
2023-03-30 16:12:07 +0200

    Componente Explicación Hecho

---
+++ src/pages/votaciones/AyudaPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <AyudaComponent></AyudaComponent>
+</template>
+  
+  <script>
+  import { ref, defineComponent } from "vue";
+  import  AyudaComponent  from "src/components/votaciones/AyudaComponent.vue";
+  
+  export default defineComponent({
+    name: "AyudaPage",
+  
+    components: {
+      AyudaComponent
+    },
+  
+    setup () {
+  
+      return {
+  
+      }
+    }
+  })
+  
+  </script>
+  
